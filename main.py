878308548b9a790b6926c4401a1cd841f5f2f8eb
RyderPoulton <ryder.poulton@marsden.school.nz>
2023-06-20 11:30:49 +1200

    Print list

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
+def print_list(L):
+    for i in range (0, len(L)):
+        output = "{} {}".format(L[i][0], L[i][1])
+        print(output)
+
+
 def sandwich_list():
     my_list = [
         ["Halloumi and apricot jam sandwich", 15.95],
         ["Banh mi with five-spice crispy pork belly, pickled carrot, chilli, coriander and cucumber", 18.95],
         ["Roasted beetroot, carrot, spiced nuts and whipped feta", 15.95],
-        ["Sausage and egg sandwich", 14.95]
+        # ["Sausage and egg sandwich", 14.95]
         # ["Smoked salmon deluxe", 15.95],
         # ["Ham sandwich in a French baguette", 16.95],
         # ["Kiwi & Roo’s ‘lucky beef’ steak sandwich", 18.95],
@@ -13,3 +19,6 @@ def sandwich_list():
         # ["Fish finger sandwich with Nordic dill salsa", 15.95],
         # ["Grilled cheddar and jalapeño popper sandwich", 15.95]
     ]
+    print_list(my_list)
+
+sandwich_list()
