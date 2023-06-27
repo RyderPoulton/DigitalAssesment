6507548d9e86768024b2473e55f8a966cfcd7884
RyderPoulton <ryder.poulton@marsden.school.nz>
2023-06-28 11:57:36 +1200

    Started delivery option

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 def print_list(L):
     for i in range(0, len(L)):
         output = "{} {}".format(L[i][0], L[i][1])
@@ -38,8 +39,7 @@ def add_sandwich(L, O):
 def review_order(L):
     for i in range(0, len(L)):
         print_order_list(L)
-        total = L[i][2]
-        print(total)
+
 
 def delete_order(L):
     L.clear()
@@ -55,13 +55,18 @@ def edit_order(L):
     print(output)
 
 
+def delivery_option():
+    option = input("Pick up (p) or delivery (d) -> ").lower()
+    if option == "p":
+
+
 def menu():
     order_list = []
     my_list = [
         ["Halloumi and apricot jam sandwich", 15.95],
         ["Banh mi with five-spice crispy pork belly, pickled carrot, chilli, coriander and cucumber", 18.95],
-        ["Roasted beetroot, carrot, spiced nuts and whipped feta", 15.95],
-        # ["Sausage and egg sandwich", 14.95]
+        ["Roasted beetroot, carrot, spiced nuts and whipped feta", 15.95]
+        # ["Sausage and egg sandwich", 14.95],
         # ["Smoked salmon deluxe", 15.95],
         # ["Ham sandwich in a French baguette", 16.95],
         # ["Kiwi & Roo’s ‘lucky beef’ steak sandwich", 18.95],
@@ -99,7 +104,7 @@ def menu():
             edit_order(order_list)
         elif choice == "Q":
             print("Program Ended")
-            run == False
+            run = False
         else:
             print("Error")
 
