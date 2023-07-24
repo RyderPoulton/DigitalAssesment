427c654f5788447403884326362c058d0e043360
RyderPoulton <ryder.poulton@marsden.school.nz>
2023-07-25 11:36:57 +1200

    5 max sandwich feature started, complete order updated (needs customer details and sandwiches in order to complete)

--- main.py
+++ main.py
@@ -26,14 +26,20 @@ def print_order_with_index(L):
 def add_sandwich(L, O):
     print_with_index(L)
     order = int(input("Enter the index number of the sandwich ordered -> "))
-    sandwich = L[order]
-    name = sandwich[0]
-    price = sandwich[1]
-    amount = int(input("Amount of sandwiches -> "))
-    new_list = [amount, name, price]
-    O.append(new_list)
-    output = "{} {} have been added to the order".format(amount, name)
-    print(output)
+    if order in L:
+        print("There are {} {} in the order".format(L[0], L[1]))
+    else:
+        sandwich = L[order]
+        name = sandwich[0]
+        price = sandwich[1]
+        amount = int(input("Amount of sandwiches -> "))
+        if amount > 5:
+            print("Too many sandwiches")
+        else:
+         new_list = [amount, name, price]
+         O.append(new_list)
+         output = "{} {} have been added to the order".format(amount, name)
+        print(output)
 
 
 def review_order(L, O):
@@ -45,7 +51,7 @@ def review_order(L, O):
         customer_details = "Name: {}".format(L[0])
         print(customer_details)
     elif len(L) == 0:
-        print("There is nothing here")
+        print("No customer details")
     else:
         print("Error")
 
@@ -80,8 +86,14 @@ def delivery_option(L):
         print("Error")
 
 
-def complete_order():
-    print("Order Complete")
+def complete_order(L, O):
+    if len(L) >= 1:
+        if len(O) >= 1:
+            print("Order Complete")
+        else:
+            print("Error")
+    else:
+        print("Error")
 
 
 def menu():
@@ -90,16 +102,16 @@ def menu():
     my_list = [
         ["Halloumi and apricot jam sandwich", 15.95],
         ["Banh mi with five-spice crispy pork belly, pickled carrot, chilli, coriander and cucumber", 18.95],
-        ["Roasted beetroot, carrot, spiced nuts and whipped feta", 15.95]
-        # ["Sausage and egg sandwich", 14.95],
-        # ["Smoked salmon deluxe", 15.95],
-        # ["Ham sandwich in a French baguette", 16.95],
-        # ["Kiwi & Roo’s ‘lucky beef’ steak sandwich", 18.95],
-        # ["Buttermilk chicken, scotch bonnet jam, pickled cabbage and crispy shallots", 18.95],
-        # ["Balik ekmek – griddled mackerel in a baguette with tomato, lettuce, onion, chilli and sumac", 18.95],
-        # ["Milanese and gremolata panini", 16.95]
-        # ["Fish finger sandwich with Nordic dill salsa", 15.95],
-        # ["Grilled cheddar and jalapeño popper sandwich", 15.95]
+        ["Roasted beetroot, carrot, spiced nuts and whipped feta", 15.95],
+        ["Sausage and egg sandwich", 14.95],
+        ["Smoked salmon deluxe", 15.95],
+        ["Ham sandwich in a French baguette", 16.95],
+        ["Kiwi & Roo’s ‘lucky beef’ steak sandwich", 18.95],
+        ["Buttermilk chicken, scotch bonnet jam, pickled cabbage and crispy shallots", 18.95],
+        ["Balik ekmek – griddled mackerel in a baguette with tomato, lettuce, onion, chilli and sumac", 18.95],
+        ["Milanese and gremolata panini", 16.95],
+        ["Fish finger sandwich with Nordic dill salsa", 15.95],
+        ["Grilled cheddar and jalapeño popper sandwich", 15.95]
      ]
     my_menu = '''
     A: Print Menu
